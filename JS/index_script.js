8a260db1bcaeef73d52fa232349cc298787a501e
Asha <paramonpav@yandex.ru>
2019-12-14 14:16:12 +0300

    Fixed minor bug

--- JS/index_script.js
+++ JS/index_script.js
@@ -1,7 +1,13 @@
 'use strict'
-var gameBody = document.getElementById("GameBody");
-const canvas = gameBody.getBoundingClientRect();
+var Container = document.getElementById("GameContainer");
+var Container_offset = Container.getBoundingClientRect();
+var can = document.getElementById("GameBody");
+const canvas = can.getBoundingClientRect();
 const imgSrc = 'IMG/Sample.jpg';
+
+
+var plane = document.getElementById("plane");
+// var missle = document.getElementById("missle");
 
 class BackGround {
     cnvs
@@ -44,19 +50,15 @@ class BackGround {
         this.CurrCoords.x += x;
         this.CurrCoords.y += y;
         if (this.loop) {
-            // if (this.CurrCoords.x < 0) { this.CurrCoords.x = this.image.width }
-            // if (this.CurrCoords.x > this.image.width) { this.CurrCoords.x = 0}
-            if (this.CurrCoords.y  < 0) { this.CurrCoords.y = this.image.height - this.imgScale.y }
-            if (this.CurrCoords.y  > this.image.height- this.imgScale.y ) { this.CurrCoords.y = 0}
-
-            
+            if (this.CurrCoords.y < 0) { this.CurrCoords.y = this.image.height - this.imgScale.y }
+            if (this.CurrCoords.y > this.image.height - this.imgScale.y) { this.CurrCoords.y = 0 }
         }
         this.cnvs.drawImage(this.image, this.CurrCoords.x, this.CurrCoords.y, this.imgScale.x, this.imgScale.y, 0, 0, canvas.width, canvas.height)
 
     }
 }
 
-var BackGrnd = new BackGround(gameBody, imgSrc, 0, 0, canvas.width/2, canvas.height/2)
+var BackGrnd = new BackGround(can, imgSrc, 0, 0, canvas.width / 2, canvas.height / 2)
 BackGrnd.Loop();
 window.setInterval(function () { BackGrnd.ShiftUp(0.01) }, 10)
 
@@ -82,3 +84,51 @@ document.addEventListener('keypress', (event) => {
     }
 })
 
+
+
+Container.addEventListener('mousedown', mouseDown)
+var Shift = {}
+var WindowShift = {}
+WindowShift.x = window.innerWidth;
+WindowShift.y = window.innerWidth;
+
+
+
+function mouseDown(event) {
+    event.preventDefault();
+    if (event.target.id == plane.id) {
+        Container_offset = Container.getBoundingClientRect();
+        Shift.x = event.clientX - plane.getBoundingClientRect().left;
+        Shift.y = event.clientY - plane.getBoundingClientRect().top;
+
+        //TODO LITLE SHIFT
+        
+        plane.style.left = event.clientX - Shift.x - Container_offset.left + 'px';
+        plane.style.top = event.clientY - Shift.y - Container_offset.top + 'px';
+
+        Container.addEventListener('mousemove', mouseMove);
+        Container.onmouseup = function () {
+            Container.removeEventListener('mousemove', mouseMove);
+            Container.onmouseup = null;
+        };
+
+    }
+
+}
+
+function mouseMove() {
+
+ 
+    plane.style.left = event.clientX - Shift.x - Container_offset.left + 'px';
+    plane.style.top = event.clientY - Shift.y - Container_offset.top + 'px';
+}
+
+
+let missle_hostile = document.getElementById('missle-hostile');
+
+setInterval(function () { missle_move(missle_hostile, 0.001) }, 10)
+
+function missle_move(target, step) {
+    target.style.top = target.getBoundingClientRect().top + step + 'px';
+
+}
